94d8659733672a241862198bdd8ed67e39450928
seyedahbr <a.beghaei@gmail.com>
2023-03-09 10:00:47 +0000

    Modified for big dumps

--- Random_Dump_Generator.py
+++ Random_Dump_Generator.py
@@ -17,19 +17,29 @@ def genargs(prog: Optional[str] = None) -> ArgumentParser:
 
 
 def extract_random_lines(file_path: str, num_lines: int, output_file: str):
-    with gzip.open(file_path, 'rt') as in_file:
-        print('Reading input file...')
-        lines = in_file.readlines()
-        print('Random sampling, following lines will be selected:')
-        random_lines = random.sample(range(1,len(lines)-1), num_lines)
-        print(random_lines)
-        with gzip.open(output_file, 'wt') as out_file:
-            print('Writing output...')
-            out_file.write('[\n')
-            for i in random_lines:
-                out_file.write(lines[i])    
-            out_file.write(']\n')
-        print('DONE')
+    in_file = gzip.open(file_path, 'rt')
+    out_file = gzip.open(output_file, 'wt')
+    print('Getting input file line numbers...')
+    lines = sum([1 for line in in_file])
+    in_file.close()
+    print('Number of lines: ', lines)
+    print('Random sampling, following lines will be selected:')
+    random_lines = random.sample(range(1, lines-1), num_lines)
+    random_lines = sorted(random_lines)
+    print(random_lines)
+    print('Writing output...')
+    out_file.write('[\n')
+    ctr = 1
+    in_file = gzip.open(file_path, 'rt')
+    for line in in_file:
+        if ctr in random_lines:
+            out_file.write(line)
+            random_lines.remove(ctr)    
+        ctr += 1
+    out_file.write(']\n')
+    in_file.close()
+    out_file.close()
+    print('DONE')
 
 def Random_Dump_Generator(argv: Optional[Union[str, List[str]]] = None, prog: Optional[str] = None) -> int:
     if isinstance(argv, str):
